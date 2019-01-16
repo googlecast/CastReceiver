00ba6367a583e4e61e23e425cf1a3616d4e78fe9
James Romo <jamesromo@google.com>
2019-01-16 11:08:42 -0800

    Added more customization and queuing.
    Change-Id: I720da8c7f152b2d0c3d437a761848031c185c3fc

---
+++ js/queuing.js
@@ -0,0 +1,86 @@
+/*
+Copyright 2018 Google LLC. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+'use strict';
+
+class CastQueue extends cast.framework.QueueBase {
+  constructor() {
+    super();
+
+    const item = new cast.framework.messages.QueueItem();
+    item.media = new cast.framework.messages.MediaInformation();
+    item.media.contentId = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    item.media.streamType = cast.framework.messages.StreamType.BUFFERED;
+    item.media.contentType = "video/mp4";
+    item.media.metadata = new cast.framework.messages.MovieMediaMetadata();
+    item.media.metadata = {
+      title: "Big Buck Bunny (2008)",
+      subtitle: "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than " +
+      "himself. When one sunny day three rodents rudely harass him, something " +
+      "snaps... and the rabbit ain't no bunny anymore! In the typical cartoon " +
+      "tradition he prepares the nasty rodents a comical revenge.",
+      images: [new cast.framework.messages.Image("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg")]
+    };
+
+    this.sampleItem = item;
+  }
+
+  /**
+  * Initializes the queue.
+  * @param {!cast.framework.messages.LoadRequestData} loadRequestData
+  * @return {!cast.framework.messages.QueueData}
+  */
+  initialize(loadRequestData) {
+    let queueData = loadRequestData.queueData;
+    // Create a new queue with media from the load request if one doesn't exist.
+    if (!queueData) {
+      queueData = new cast.framework.messages.QueueData();
+      let item = new cast.framework.messages.QueueItem();
+      item.media = loadRequestData.media;
+      queueData.items = [item];
+    }
+
+    return queueData;
+ }
+
+  /**
+  * Picks a set of items after the reference item id and returns as the next
+  * items to be inserted into the queue. When referenceItemId is omitted, items
+  * are simply appended to the end of the queue.
+  * @param {number} referenceItemId
+  * @return {!Array<cast.framework.QueueItem>}
+  */
+  nextItems(referenceItemId) {
+    // Return sample content.
+    return [this.sampleItem];
+  }
+
+  /**
+  * Picks a set of items before the reference item id and returns as the items
+  * to be inserted into the queue. WhenvreferenceItemId is omitted, items are
+  * simply appended to beginning of the queue.
+  * @param {number} referenceItemId
+  * @return {!Array<cast.framework.QueueItem>}
+  */
+  prevItems(referenceItemId) {
+    // Return sample content.
+    return [this.sampleItem];
+  }
+};
+
+export {
+  CastQueue
+}
--- js/receiver.js
+++ js/receiver.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Google Inc. All Rights Reserved.
+Copyright 2018 Google LLC. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -20,14 +20,28 @@ Cast.
 
 'use strict';
 
+import { CastQueue } from './queuing.js';
+
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
-// Listen and log all Core Events
+// Listen and log all Core Events.
 playerManager.addEventListener(cast.framework.events.category.CORE,
   event => {
     console.log("Core event: " + event.type);
     console.log(event);
+  });
+
+playerManager.addEventListener(
+  cast.framework.events.EventType.MEDIA_STATUS, (event) => {
+    console.log("MEDIA_STATUS event: " + event.type);
+    console.log(event);
+});
+
+// Intercept the LOAD request to be able to read in a contentId and get data.
+playerManager.setMessageInterceptor(
+  cast.framework.messages.MessageType.LOAD, loadRequestData => {
+    return loadRequestData;
   });
 
 const playbackConfig = new cast.framework.PlaybackConfig();
@@ -36,4 +50,34 @@ const playbackConfig = new cast.framework.PlaybackConfig();
 // media content buffered. Default is 10.
 playbackConfig.autoResumeDuration = 5;
 
-context.start();
+// Set the available buttons in the UI controls.
+const controls = cast.framework.ui.Controls.getInstance();
+controls.clearDefaultSlotAssignments();
+
+// Assign buttons to control slots.
+controls.assignButton(
+  cast.framework.ui.ControlsSlot.SLOT_1,
+  cast.framework.ui.ControlsButton.QUEUE_PREV
+)
+controls.assignButton(
+  cast.framework.ui.ControlsSlot.SLOT_2,
+  cast.framework.ui.ControlsButton.CAPTIONS
+)
+controls.assignButton(
+  cast.framework.ui.ControlsSlot.SLOT_3,
+  cast.framework.ui.ControlsButton.SEEK_FORWARD_15
+)
+controls.assignButton(
+  cast.framework.ui.ControlsSlot.SLOT_4,
+  cast.framework.ui.ControlsButton.QUEUE_NEXT
+)
+
+context.start({
+  queue: new CastQueue(),
+  playbackConfig: playbackConfig,
+  supportedCommands: cast.framework.messages.Command.ALL_BASIC_MEDIA |
+                      cast.framework.messages.Command.QUEUE_PREV |
+                      cast.framework.messages.Command.QUEUE_NEXT
+});
+
+
