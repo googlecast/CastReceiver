e241772d5661204210c90e72a878ec62e92e9748
Vishal Punjabi <vpunjabi@vpunjabi-macbookpro.roam.corp.google.com>
2020-04-03 16:38:45 -0700

    Usage of Debug logger
    Change-Id: If845fc51856d9c7144b15c535991d71404b729dd

--- js/queuing.js
+++ js/queuing.js
@@ -16,23 +16,45 @@ limitations under the License.
 
 'use strict';
 
+const LOG_QUEUE_TAG = 'Queue';
+
+/**
+ * Debug Logger
+ */
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+
+if (!castDebugLogger.loggerLevelByTags) {
+  castDebugLogger.loggerLevelByTags = {};
+}
+/**
+ * Set verbosity level for custom tag.
+ * Enable log messages for error, warn and info.
+ */
+castDebugLogger.loggerLevelByTags[LOG_QUEUE_TAG] =
+  cast.framework.LoggerLevel.INFO;
+
 class CastQueue extends cast.framework.QueueBase {
   constructor() {
     super();
 
     const item = new cast.framework.messages.QueueItem();
     item.media = new cast.framework.messages.MediaInformation();
-    item.media.contentId = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    item.media.contentUrl = 'https://commondatastorage.googleapis.com/' + 
+      'gtv-videos-bucket/sample/BigBuckBunny.mp4';
     item.media.streamType = cast.framework.messages.StreamType.BUFFERED;
-    item.media.contentType = "video/mp4";
+    item.media.contentType = 'video/mp4';
     item.media.metadata = new cast.framework.messages.MovieMediaMetadata();
-    item.media.metadata.title = "Big Buck Bunny (2008)";
-    item.media.metadata.subtitle = "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than " +
-        "himself. When one sunny day three rodents rudely harass him, something " +
-        "snaps... and the rabbit ain't no bunny anymore! In the typical cartoon " +
-        "tradition he prepares the nasty rodents a comical revenge.";
+    item.media.metadata.title = 'Big Buck Bunny (2008)';
+    item.media.metadata.subtitle = 'Big Buck Bunny tells the story of a ' +
+      'giant rabbit with a heart bigger than himself. When one sunny day ' +
+      'three rodents rudely harass him, something snaps... and the rabbit ' +
+      'ain\'t no bunny anymore! In the typical cartoon tradition he prepares ' +
+      'the nasty rodents a comical revenge.';
     item.media.metadata.images =
-        [new cast.framework.messages.Image("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg")];
+        [new cast.framework.messages.Image(
+          'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/' + 
+            'images/BigBuckBunny.jpg'
+        )];
 
     this.sampleItem = item;
   }
@@ -43,16 +65,20 @@ class CastQueue extends cast.framework.QueueBase {
   * @return {!cast.framework.messages.QueueData}
   */
   initialize(loadRequestData) {
-    let queueData = loadRequestData.queueData;
-    // Create a new queue with media from the load request if one doesn't exist.
-    if (!queueData) {
-      queueData = new cast.framework.messages.QueueData();
-      let item = new cast.framework.messages.QueueItem();
-      item.media = loadRequestData.media;
-      queueData.items = [item];
-    }
+    if (loadRequestData) {
+      let queueData = loadRequestData.queueData;
 
-    return queueData;
+      // Create a new queue with media from load request if one doesn't exist.
+      if (!queueData) {
+        castDebugLogger.info(LOG_QUEUE_TAG,
+          'Creating a new queue with media from the load request');
+        queueData = new cast.framework.messages.QueueData();
+        let item = new cast.framework.messages.QueueItem();
+        item.media = loadRequestData.media;
+        queueData.items = [item];
+      }
+      return queueData;
+    }
  }
 
   /**
@@ -82,4 +108,4 @@ class CastQueue extends cast.framework.QueueBase {
 
 export {
   CastQueue
-}
+};
--- js/receiver.js
+++ js/receiver.js
@@ -13,10 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-/*
-This sample demonstrates how to build your own Receiver for use with Google
-Cast.
-*/
+
+/**
+ * This sample demonstrates how to build your own Receiver for use with Google
+ * Cast.
+ */
 
 'use strict';
 
@@ -25,52 +26,122 @@ import { CastQueue } from './queuing.js';
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
-// Listen and log all Core Events.
-playerManager.addEventListener(cast.framework.events.category.CORE,
-  event => {
-    console.log("Core event: " + event.type);
-    console.log(event);
-  });
 
+const LOG_RECEIVER_TAG = 'Receiver';
+
+/**
+ * Debug Logger
+ */
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+
+/**
+ * WARNING: Make sure to turn off debug logger for production release as it
+ * may expose details of your app.
+ * Uncomment below line to enable debug logger and show a 'DEBUG MODE' tag at
+ * top left corner.
+ */
+// castDebugLogger.setEnabled(true);
+
+/**
+ * Uncomment below line to show debug overlay.
+ */
+// castDebugLogger.showDebugLogs(true);
+
+/**
+ * Set verbosity level for Core events.
+ */
+castDebugLogger.loggerLevelByEvents = {
+  'cast.framework.events.category.CORE': 
+    cast.framework.LoggerLevel.INFO,
+  'cast.framework.events.EventType.MEDIA_STATUS': 
+    cast.framework.LoggerLevel.DEBUG
+};
+
+if (!castDebugLogger.loggerLevelByTags) {
+  castDebugLogger.loggerLevelByTags = {};
+}
+
+/**
+ * Set verbosity level for custom tag.
+ * Enables log messages for error, warn, info and debug.
+ */
+castDebugLogger.loggerLevelByTags[LOG_RECEIVER_TAG] = 
+  cast.framework.LoggerLevel.DEBUG;
+
+/**
+ * Example of how to listen for events on playerManager.
+ */
 playerManager.addEventListener(
-  cast.framework.events.EventType.MEDIA_STATUS, (event) => {
-    console.log("MEDIA_STATUS event: " + event.type);
-    console.log(event);
+  cast.framework.events.EventType.ERROR, (event) => {
+    castDebugLogger.error(LOG_RECEIVER_TAG,
+      'Detailed Error Code - ' + event.detailedErrorCode);
+    if (event && event.detailedErrorCode == 905) {
+      castDebugLogger.error(LOG_RECEIVER_TAG,
+        'LOAD_FAILED: Verify the load request is set up ' +
+        'properly and the media is able to play.');
+    }
 });
 
-// Intercept the LOAD request to be able to read in a contentId and get data.
+/**
+ * Intercept the LOAD request to be able to read in a contentId and get data.
+ */
 playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD, loadRequestData => {
+    castDebugLogger.debug(LOG_RECEIVER_TAG,
+      `LOAD interceptor loadRequestData: ${JSON.stringify(loadRequestData)}`);
+    if (!loadRequestData || !loadRequestData.media) {
+      const error = new cast.framework.messages.ErrorData(
+        cast.framework.messages.ErrorType.LOAD_FAILED);
+      error.reason = cast.framework.messages.ErrorReason.INVALID_REQUEST;
+      castDebugLogger.error(LOG_RECEIVER_TAG, 'Invalid load request');
+      return error;
+    }
+    if (!loadRequestData.media.contentUrl) {
+      castDebugLogger.warn(LOG_RECEIVER_TAG,
+        'Playable URL is missing. Using ContentId as a fallback.');
+      if (!loadRequestData.media.contentId) {
+        castDebugLogger.error(LOG_RECEIVER_TAG,
+          'Missing Content ID and Playable URL.');
+      }
+    }
     return loadRequestData;
   });
 
 const playbackConfig = new cast.framework.PlaybackConfig();
 
-// Set the player to start playback as soon as there are five seconds of
-// media content buffered. Default is 10.
+/**
+ * Set the player to start playback as soon as there are five seconds of
+ * media content buffered. Default is 10.
+ */
 playbackConfig.autoResumeDuration = 5;
+castDebugLogger.info(LOG_RECEIVER_TAG,
+  `autoResumeDuration set to: ${playbackConfig.autoResumeDuration}`);
 
-// Set the available buttons in the UI controls.
+/**
+ * Set the control buttons in the UI controls.
+ */
 const controls = cast.framework.ui.Controls.getInstance();
 controls.clearDefaultSlotAssignments();
 
-// Assign buttons to control slots.
+/**
+ * Assign buttons to control slots.
+ */
 controls.assignButton(
   cast.framework.ui.ControlsSlot.SLOT_1,
   cast.framework.ui.ControlsButton.QUEUE_PREV
-)
+);
 controls.assignButton(
   cast.framework.ui.ControlsSlot.SLOT_2,
   cast.framework.ui.ControlsButton.CAPTIONS
-)
+);
 controls.assignButton(
   cast.framework.ui.ControlsSlot.SLOT_3,
   cast.framework.ui.ControlsButton.SEEK_FORWARD_15
-)
+);
 controls.assignButton(
   cast.framework.ui.ControlsSlot.SLOT_4,
   cast.framework.ui.ControlsButton.QUEUE_NEXT
-)
+);
 
 context.start({
   queue: new CastQueue(),
@@ -79,5 +150,3 @@ context.start({
                       cast.framework.messages.Command.QUEUE_PREV |
                       cast.framework.messages.Command.QUEUE_NEXT
 });
-
-
