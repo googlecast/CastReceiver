8428d38ceccdaf0788cb2f9c115a810af310beeb
Vincent Zhou <fireflysuccess@gmail.com>
2020-04-18 08:44:38 +0800

    Use stomp web socket for amqp
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- js/receiver.js
+++ js/receiver.js
@@ -22,6 +22,91 @@ Cast.
 
 import { CastQueue } from './queuing.js';
 
+    const client = new StompJs.Client({
+      brokerURL: "ws://localhost:15674/ws",
+      connectHeaders: {
+        login: "guest",
+        passcode: "guest"
+      },
+      debug: function (str) {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000
+    });
+    
+  client.debug = function(str) {
+    console.log(str);
+  };
+
+    client.onConnect = function(frame) {
+      // Do something, all subscribes must be done is this callback
+      // This is needed because this will be executed after a (re)connect
+    };
+    
+    client.onStompError = function (frame) {
+      // Will be invoked in case of error encountered at Broker
+      // Bad login/passcode typically will cause an error
+      // Complaint brokers will set `message` header with a brief message. Body may contain details.
+      // Compliant brokers will terminate the connection after any error
+      console.log('Broker reported error: ' + frame.headers['message']);
+      console.log('Additional details: ' + frame.body);
+    };
+    
+    client.activate();
+
+function generateBinaryData() {
+    let buffer = new Int8Array(16);
+    return buffer
+}
+    var binaryData = generateBinaryData(); // This need to be of type Uint8Array
+    // setting content-type header is not mandatory, however a good practice
+    client.publish({destination: '', binaryBody: binaryData,
+                    headers: {'content-type': 'application/octet-stream'}});
+//    client.publish({destination: '/topic/general', body: 'Hello world'});
+//
+//    // There is an option to skip content length header
+    client.publish({destination: '/example', body: 'Hello world', skipContentLengthHeader: true});
+//    
+//    // Additional headers
+//    client.publish({destination: '/topic/general', body: 'Hello world', headers: {'priority': '9'}});
+
+
+  var callback = function(message) {
+    // called when the client receives a STOMP message from the server
+    if (message.body) {
+      alert("got message with body " + message.body)
+    } else {
+      alert("got empty message");
+    }
+  };
+  var subscription = client.subscribe("/queue/test", callback);
+
+// Create a socket instance
+//var socket = new WebSocket('ws://localhost:61613');
+//
+//// Open the socket
+//socket.onopen = function(event) {
+//
+//    // Send an initial message
+//    socket.send('I am the client and I\'m listening!');
+//
+//    // Listen for messages
+//    socket.onmessage = function(event) {
+//        console.log('Client received a message',event);
+//    };
+//
+//    // Listen for socket closes
+//    socket.onclose = function(event) {
+//        console.log('Client notified socket has closed',event);
+//    };
+//
+//    // To close the socket....
+//    socket.close()
+//
+//};
+
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
 
